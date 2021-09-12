0f0c94a0cc8b3b3ed55ef34c247a4c7e318e7600
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-09-12 07:56:31 +0530

    puppet

--- Puppet/puppet programs/NTP/NTP.pp
+++ Puppet/puppet programs/NTP/NTP.pp
@@ -1,15 +1,22 @@
+#Definition of a class
+class ntp {
+
 # NTP package installation
-package { "ntp":
-  ensure => "present",
-}
+   package { "ntp":
+     ensure => "present",
+   }
 
 #NTP file configuration
-file { "/etc/ntp.conf":
-   ensure  => "present",
-   content => "server 0.centos.ntp.org iburst\n",
-}
+   file { "/etc/ntp.conf":
+     ensure  => "present",
+     content => "server 0.centos.ntp.org iburst\n",
+   }
 
 #NTP service startup
-service { "ntpd": 
-  ensure => "runnning",
+  service { "ntpd": 
+    ensure => "running",
+   }
 }
+
+#Declaration of a class
+include ntp
